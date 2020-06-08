30749eee94615353f2170246cc1175a561cd6b15
神崎裕輔 <hulkmining@gmail.com>
2020-06-09 07:25:11 +0900

    no message

--- 7-18.php
+++ 7-18.php
@@ -1,29 +1,35 @@
 <?php
 
 class AryRmv {
-    public function aryary($c, $idx) {
-        unset($c[$idx]);
-        var_dump($c);
-        return $c;
+    public function aryary($a, $idx, $b) {
+        if ($idx >= 0 && $idx < $b) {
+            for ($i = $idx; $i <= $b - 1; $i++) { 
+                $a[$i] = $a[$i + 1];
+                var_dump($a);
+            }
+            if ($idx == $b - 1) {
+                # code...
+            }
+            return $a;
+        }
     }
 }
 
 $ary = new AryRmv;
 echo '要素数は？：';
-$b = trim(fgets(STDIN));
-$c = array_fill(0, $b, 0);
-for ($i = 0; $i < $b; $i++) { 
-    echo '要素の値は？：';
-    $c[$i] = (int)trim(fgets(STDIN));
-}
-var_dump($c);
+$num = trim(fgets(STDIN));
+$a = array_fill(0, $num, 0);
+$b = count($a);
 
-// foreach ($c as $value) {
-//     echo $value. PHP_EOL;
-// }
+for ($i = 0; $i < $num; $i++) { 
+    echo '$a['. $i. '] : ';
+    $a[$i] = trim(fgets(STDIN));
+}
+
 echo '削除するインデックスは？：';
 $idx = trim(fgets(STDIN));
-$ary->aryary($c ,$idx);
-foreach ($c as $keykey) {
-    echo $keykey. PHP_EOL;
+
+$a = $ary->aryary($a ,$idx, $b);
+foreach ($a as $value) {
+    echo $value. PHP_EOL;
 }
