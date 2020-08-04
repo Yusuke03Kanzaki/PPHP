c9c3dcb265e9d35fb08e2805b3ca5ba85e3c4030
Yusuke Kanzaki <hulkmining@gmail.com>
2020-08-04 21:27:53 +0900

    commit

--- 9-1_Tester2.php
+++ 9-1_Tester2.php
@@ -4,7 +4,7 @@ require('9-1_class.php');
 class CoordinateTester2 extends Coordinate {
 
     static function compCoordinate($c1, $c2) {
-        var_dump($c1, $c2);
+        // var_dump($c1, $c2);
         return self::getX($c1) == self::getX($c2) && self::getY($c1) == self::getY($c2);
     }
 }
@@ -28,5 +28,5 @@ $q = new CoordinateTester2($x, $y);
 echo ($p == $q ? 'p == qです。' : 'p != qです。'). PHP_EOL;
 echo ($p->getX() == $q->getX() && $p->getY() == $q->getY() ? 'pとqは等しいです。' : 'pとqは等しくありません。'). PHP_EOL; //pとqは等しいです。なぜ？
 echo (CoordinateTester2::compCoordinate($p, $q) ? 'pとqは等しいです。' : 'pとqは等しくありません。'). PHP_EOL; //pとqは等しいです。なぜ？
-echo $p->x. $p->y. PHP_EOL; //11
-echo $q->y. $q->y. PHP_EOL; //00
+// echo $p->x. $p->y. PHP_EOL; //11
+// echo $q->y. $q->y. PHP_EOL; //00
